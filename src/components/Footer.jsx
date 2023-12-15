576166b9a98174f5f252f9e3aba7e4b4f4be60f3
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-12-15 12:03:22 +0100

    refactor: add copyrights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className="footer flex flex-col items-center justify-start px-[5rem] bg-[#081730] h-[45rem] pt-[18rem] mt-[-10rem] relative z-[-1]">
         <CenterMenu/>
         {/* Social Icons */}
-        <div className="flex w-[100%] justify-center mt-14">
+        <div className="flex w-[100%] justify-center mt-14 text-center">
             <div className={SocialStyle}>
             <a href="https://web.facebook.com/g.a.gbaguidi">
               <Facebook/>
@@ -27,7 +27,7 @@ function Footer() {
       {/* details */}
       
         <span className="text-[1rem] text-gray-400 px-[15rem] text-center mt-[4rem]">
-         &copy; HarmoniMix 2023
+        &copy; HarmoniMix 2023, Owrn by Guy Ahonakpon GBAGUIDI
         </span>
       
     </div>
